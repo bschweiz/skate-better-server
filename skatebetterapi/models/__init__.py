69ddb9e64ea48de2753c4078abc806cd8c842741
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-12 14:40:14 -0600

    i think i got the models finished, not sure about all the on_delete parameters for the properties, need to read the docs on that

--- skatebetterapi/models/__init__.py
+++ skatebetterapi/models/__init__.py
@@ -1,5 +1,5 @@
 from .skater import Skater
 from .trick import Trick
 from .game import Game
-from .gameTrick import GameTrick
+from .gametrick import GameTrick
 from .opponent import Opponent
--- skatebetterapi/models/trick.py
+++ skatebetterapi/models/trick.py
@@ -1,11 +1,9 @@
 from django.db import models
 from .skater import Skater
-from .gametype import GameType
 
 class Trick(models.Model):
 
-    skater = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    game_type = models.ForeignKey("GameType", on_delete= models.CASCADE)
+    skater = models.ForeignKey(Skater, on_delete=models.CASCADE)
     name = models.CharField(max_length=125)
     stance = models.IntegerField()
     description = models.CharField(max_length=333)
