dba32001fd1aba125524c5640d575c2f143477e9
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-12 16:29:56 -0600

    Im too tired and need to step away, on the verge of starting to make stupid mistakes. Debugged typo on models/opponent.py then I realized that I never migrated my custom models so the Skater Table does not even exist yet to put user data on to. STILL NEED TO DO MIGRATIONS. later tonight or tomorrow.

--- skatebetterapi/models/opponent.py
+++ skatebetterapi/models/opponent.py
@@ -3,7 +3,7 @@ from django.db import models
 from .skater import Skater
 
 
-class Skater(models.Model):
+class Opponent(models.Model):
 
     skater = models.ForeignKey(Skater, on_delete=models.CASCADE)
     handle = models.CharField(max_length=50)
