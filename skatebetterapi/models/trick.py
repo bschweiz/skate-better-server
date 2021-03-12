be206d3c938b1394a99b402c7867a44b494619c8
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-12 14:35:54 -0600

    only gameTrick left to do

--- skatebetterapi/models/trick.py
+++ skatebetterapi/models/trick.py
@@ -7,4 +7,9 @@ class Trick(models.Model):
     skater = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     game_type = models.ForeignKey("GameType", on_delete= models.CASCADE)
     name = models.CharField(max_length=125)
+    stance = models.IntegerField()
     description = models.CharField(max_length=333)
+
+
+# stance will be as follows:
+# 1 = reg, 2 = fakie, 3 = switch, 4 = nollie
