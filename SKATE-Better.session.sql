ff68aa8918fba58802e78ac59730d2385b1f9909
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-15 10:13:19 -0500

    successfully generating SKATER table data along with the User Table

--- SKATE-Better.session.sql
+++ SKATE-Better.session.sql
@@ -1,2 +1,4 @@
-SELECT username, first_name, last_name
-FROM auth_user
+SELECT username, first_name, last_name, handle, goofy, fav_skater
+FROM auth_user au
+JOIN skatebetterapi_skater sk
+ON au.id = sk.user_id
