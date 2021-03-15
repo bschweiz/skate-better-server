64e9cc6f99980b9ff31a8d5224e2d95990e5b1b7
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-15 10:14:13 -0500

    tested stance boolean and it works

--- SKATE-Better.session.sql
+++ SKATE-Better.session.sql
@@ -1,4 +1,5 @@
-SELECT username, first_name, last_name, handle, goofy, fav_skater
+SELECT username, first_name, last_name, 
+        handle, goofy, fav_skater, fav_video
 FROM auth_user au
 JOIN skatebetterapi_skater sk
 ON au.id = sk.user_id
