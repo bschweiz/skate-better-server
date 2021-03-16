1446a2b236c15361d88c15d0f65f747c489f3136
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-16 11:00:49 -0500

    working on list verb

--- skatebetterapi/views/opponent.py
+++ skatebetterapi/views/opponent.py
@@ -8,47 +8,33 @@ from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from skatebetterapi.models import Skater, Game
+from skatebetterapi.models import Skater, Game, Opponent
 
-class Skaters(ViewSet):
-    # handle GET request to profile resource, returns JSON of User info and events
-    # def list(self, request):
-    #     skater = Skater.objects.get(user=request.auth.user)
-    #     games = Game.objects.filter(games__skater=skater)
-
-    #     games = GameSerializer(
-    #         events, many=True, context={'request': request})
-    #     skater = SkaterSerializer(
-    #         skater, many=False, context={'request': request})
-    #     # there is NO MODEL for Profile so we gotta make an obj fr. scatch
-    #     profile = {}
-    #     profile['skater'] = skater.data
-    #     profile['games'] = games.data
-
-    #     return Response(profile)
-
+class Opponents(ViewSet):
+   
     
 # gonna try and build a simple user data responder
     def list(self, request):
         try:
             skater = Skater.objects.get(user=request.auth.user)
+            opponent = Opponent.objects.filter(skater=skater)
             
         
             # games = GameSerializer(games, many=True, context={'context': request})
             skater.games = Game.objects.filter(skater=skater)
 
-            profile = ProfileSerializer(skater, many=False, context={'context': request})
+            opponent = OpponentSerializer(opponent, many=True, context={'context': request})
 
             return Response(profile.data)
             
         except Exception as ex:
             return HttpResponseServerError(ex, status=status.HTTP_404_NOT_FOUND)
         
-class UserSerializer(serializers.ModelSerializer):
+class OpponentSerializer(serializers.ModelSerializer):
     # JSON serializer for gamer's related DJANGO 'User'
     class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'username')
+        model = Opponent
+        fields = ('handle', 'goofy')
 
 class GameSerializer(serializers.ModelSerializer):
     # JSON serializer for gamer's related DJANGO 'User'
