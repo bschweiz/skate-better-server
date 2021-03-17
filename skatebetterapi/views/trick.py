c2755b380708060f2e1b519dff954ea6c076ed26
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-17 10:11:46 -0500

    working on serializer

--- skatebetterapi/views/trick.py
+++ skatebetterapi/views/trick.py
@@ -11,29 +11,12 @@ from rest_framework import serializers
 from skatebetterapi.models import Skater, Game, Opponent
 
 class Tricks(ViewSet):
-    def create(self, request):
-        # handle POST operations for events, returns serialized JSON instance
-        opponent = Opponent()
-        opponent.skater = Skater.objects.get(user=request.auth.user)
-        opponent.goofy = request.data['goofy']
-        opponent.handle = request.data['handle']
-        
-
-        try:
-            opponent.save()
-            serializer = OpponentSerializer(opponent, context={'request': request})
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except ValidationError as ex:
-            return Response({'reason': ex.message}, status=status.HTTP_400_BAD_REQUEST)
-# gonna try and build a simple user data responder
+    
     def list(self, request):
         try:
-            skater = Skater.objects.get(user=request.auth.user)
-            opponent = Opponent.objects.filter(skater=skater)
+            tricks = Trick.objects.all()
             
         
-            # games = GameSerializer(games, many=True, context={'context': request})
-            skater.games = Game.objects.filter(skater=skater)
 
             opponent = OpponentSerializer(opponent, many=True, context={'context': request})
 
@@ -42,8 +25,8 @@ class Tricks(ViewSet):
         except Exception as ex:
             return HttpResponseServerError(ex, status=status.HTTP_404_NOT_FOUND)
         
-class OpponentSerializer(serializers.ModelSerializer):
+class TrickSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model = Opponent
-        fields = ('handle', 'goofy')
+        model = Trick
+        fields = ('id','name', 'goofy')
