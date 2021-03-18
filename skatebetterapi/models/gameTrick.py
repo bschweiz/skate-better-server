257a9f16e0883cee49c901609ca7e3f361eaa00f
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-18 15:05:33 -0500

    one games worth of gametrick entities ready to load onto the table in migrations

--- skatebetterapi/models/gameTrick.py
+++ skatebetterapi/models/gameTrick.py
@@ -6,3 +6,5 @@ class GameTrick(models.Model):
 
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     trick = models.ForeignKey(Trick, on_delete=models.DO_NOTHING)
+    user_make = models.BooleanField(default=True)
+    opponent_make = models.BooleanField(default=True)
